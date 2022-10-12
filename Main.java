ac0f315fbb72c21deb64d73e50a485bed9465af6
Elfving2 <112498823+Elfving2@users.noreply.github.com>
2022-10-12 21:09:46 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,54 +1,41 @@
 import java.util.Scanner;
 
 class main {
-  public static void main(String[] args) {
-    //Generera random nummer
-    int random = (int) Math.round(Math.random() * 100);
-    System.out.println(random);
+public static void main(String [] args) {
+  int random = (int)Math.round(Math.random() * 100);
+  System.out.println(random);
+  int numberOfGuesses = 0;
+  System.out.println("Enter a number 0 - 100");
 
-    //boolean som ska bli false när spelaren har gissat rätt.
-    boolean numberNotCorrect = true;
+  Scanner sc = new Scanner(System.in);
 
-    //antal gissningar
-    int guesses = 0;
-
-    //skapa user input
-    Scanner newScanner = new Scanner(System.in);
-
-    //jämföra nummer och input
-    while (numberNotCorrect) {
-      System.out.println(numberNotCorrect);
-      //sätta scanner till att gissa integer nummer.
-      int userInput = newScanner.nextInt();
-      //användarens tal är större än random numret.
-      if (userInput > random) {
-        guesses++;
-        System.out.println("Nummer är lägre");
-      }
-      //användarens tal är mindre än random numret
-      else if (userInput < random) {
-        guesses++;
-        System.out.println("Nummer är större");
-      }
-      //Ifall användaren gissar rätt
-      else {
-        guesses++;
-        System.out.println("Antal gissningar: " + guesses);
-        System.out.println("Du gissa rätt!");
-        cheekifcorrect(numberNotCorrect);
-        //System.out.println(cheekifcorrect(numberNotCorrect,numberCorrect));
-        //reset
-        guesses = 0;
-        //reset
-        random = (int) Math.round(Math.random() * 100);
-        System.out.println(random);
-
-      }
+  while(true) {
+    numberOfGuesses ++;
+    int userInput = sc.nextInt();
+    System.out.println(numberGuessingGame(random,userInput));
+    while(random == userInput) {
+      System.out.println("Times guessed was " +  numberOfGuesses + " times");
+      numberOfGuesses = 0;
+      System.out.println("Play again by typing a new number!");
+      random = (int)Math.round(Math.random() * 100);
+      //System.out.println(random);
     }
+
   }
 
-  static boolean cheekifcorrect(boolean numberNotCorrect) {
-   numberNotCorrect = false;
-    return numberNotCorrect;
+
+}
+
+
+  static String numberGuessingGame(int random, int userInput) {
+    if(userInput == random) {
+      return "Correct!";
+    }
+    else if(userInput < random) {
+      return "Number is Bigger";
+    }
+    else{
+      return "Number is Smaller";
+    }
   }
 }
